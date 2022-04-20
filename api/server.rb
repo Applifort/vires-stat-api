e2d84387269557fc351d41d70e0a92a30bb97e98
Artur Gabitov <artur.gabitov@dualbootpartners.com>
2022-04-20 19:53:47 +0500

    add redis requrest

--- api/server.rb
+++ api/server.rb
@@ -1,11 +1,11 @@
+require './api/redis_db'
+
 Handler = Proc.new do |req, res|
+
+  client = RedisDb.new
+  client.set('first', 'value')
 
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
   res.body = "OK"
-<<<<<<< HEAD
-  # changes
-=======
-
->>>>>>> c789a3f... refactoring
 end
