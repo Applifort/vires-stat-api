7d99d9f9172108c228afe55606d3da9c30c6734c
Artur Gabitov <artur.gabitov@dualbootpartners.com>
2022-04-20 14:18:41 +0500

    refactoring

--- api/server.rb
+++ api/server.rb
@@ -3,5 +3,9 @@ Handler = Proc.new do |req, res|
   res.status = 200
   res['Content-Type'] = 'text/text; charset=utf-8'
   res.body = "OK"
+<<<<<<< HEAD
   # changes
+=======
+
+>>>>>>> c789a3f... refactoring
 end
